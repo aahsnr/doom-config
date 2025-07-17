d6cef6b0f42e56a52b54a9f6de8deb69cd23bbeb
aahsnr <ahsanur041@proton.me>
2025-07-17 13:22:03 +0600

    first commit

---
+++ packages.el
@@ -0,0 +1,45 @@
+;;; package --- Summary
+;;; -*- lexical-binding: t; -*-
+
+;; Theme & UI
+(package! catppuccin-theme)
+(package! mixed-pitch)
+(package! visual-fill-column)
+(package! info-colors)
+(package! colorful-mode)
+(package! rainbow-identifiers)
+
+;; Evil
+(package! evil-goggles)
+(package! evil-escape)
+(package! evil-cleverparens
+  :recipe (:host github :repo "tomdl89/evil-cleverparens" :branch "fix/delete-escaped-parens"))
+(package! paredit)
+
+;; Org & Roam
+(package! org-roam-ui)
+(package! doct)
+(package! evil-org)
+(package! org-super-agenda)
+(package! org-fragtog) ; For toggling LaTeX fragment previews
+
+;; Citations & LaTeX
+(package! citar-org-roam)
+(package! org-roam-bibtex)
+(package! consult-bibtex
+  :recipe (:host github :repo "mohkale/consult-bibtex"))
+
+;; Development & System
+(package! embark-vc)
+(package! multi-vterm)
+(package! feature-mode)
+(package! systemd)
+(package! rpm-spec-mode
+  :recipe (:host github :repo "bhavin192/rpm-spec-mode"))
+(package! eglot-booster
+  :recipe (:host github :repo "jdtsmith/eglot-booster"))
+
+;; Dependencies for LSP/Org-Jupyter
+(package! zmq)
+(package! websocket)
+(package! simple-httpd)
